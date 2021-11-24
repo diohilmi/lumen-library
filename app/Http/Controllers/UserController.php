670696edb207d3ea54f2575562b6612a9357e0c4
diohilmi <diohilmi98@gmail.com>
2021-11-24 12:05:23 +0700

    merger update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,11 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
 
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Database\QueryException;
-
 class UserController extends Controller
 {
     /**
